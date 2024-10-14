f9e02268eb2856f3f9b85a4199f0eba4dfb40bb6
turk <turk@grackle.club>
2024-10-13 18:31:42 -0600

    allow read from local or embed filesystem

--- migration.go
+++ migration.go
@@ -1,76 +1,60 @@
 package postgres
 
 import (
+	"embed"
 	"fmt"
-	"log/slog"
-	"os"
+	"io/fs"
 	"path"
-	"strings"
 )
 
-// Migrations represents a single SQL migration file,
-// including the direction, file name, and content
+//go:embed example-migrations
+var migrationsDirEmbed embed.FS
+
+const exampleMigrationsDir = "example-migrations"
+
+// Migrations represents a single SQL migration file.
 type Migration struct {
 	Direction string
 	Filename  string
 	Content   string
 }
 
-// Migrations reads the migrations directory for files matching
-// the direction, returning an error if no files are found or
-// returning a list of Migration objects containing the file name and contents
-func Migrations(migrationsDir string, direction string) ([]Migration, error) {
-	switch direction {
-	case "up":
-		slog.Debug("fetching up migrations", "dir", migrationsDir)
-	case "down":
-		slog.Debug("fetching down migrations", "dir", migrationsDir)
-	default:
-		return nil, fmt.Errorf(
-			"invalid direction '%s', must be 'up' or 'down'",
-			direction,
-		)
-	}
-
-	// read directory
-	files, err := os.ReadDir(migrationsDir)
+// Migrations reads an arbirary file system to return
+// a list of "up" or "down" migration SQL objects with:
+//   - filename
+//   - content
+//   - direction
+//
+// # local
+//
+//	m, err := Migrations(os.DirFS("."), "migrations-dir-name", "up")
+//
+// # embed
+//
+//	//go:embed migrations
+//	var migrationsDirEmbed embed.FS
+//	migrations, err := Migrations(migrationsDirEmbed, "migrations-dir-name", "up")
+func Migrations(fsys fs.FS, directory string, direction string) ([]Migration, error) {
+	entries, err := fs.ReadDir(fsys, directory)
 	if err != nil {
-		return nil, fmt.Errorf("unable to read directory '%s': %w",
-			migrationsDir,
-			err,
-		)
+		return nil, fmt.Errorf("unable to read directory %q: %w", directory, err)
 	}
-	if len(files) == 0 {
-		return nil, fmt.Errorf("no files found in directory '%s'", migrationsDir)
-	}
-
-	// capture data
 	var migrations []Migration
-	for _, file := range files {
-		if file.IsDir() {
-			slog.Warn("skipping directory", "name", file.Name())
+	for _, entry := range entries {
+		if entry.IsDir() {
 			continue
 		}
-		expectedEnding := fmt.Sprintf("%s.sql", direction)
-		if strings.Contains(file.Name(), expectedEnding) {
-			bytes, err := os.ReadFile(path.Join(migrationsDir, file.Name()))
-			if err != nil {
-				return nil, fmt.Errorf("unable to read file '%s': %w", file.Name(), err)
-			}
-			if len(bytes) == 0 {
-				return nil, fmt.Errorf("empty file '%s'", file.Name())
-			}
-			migration := Migration{
-				Direction: direction,
-				Filename:  file.Name(),
-				Content:   string(bytes),
-			}
-			migrations = append(migrations, migration)
+		content, err := fs.ReadFile(fsys, path.Join(directory, entry.Name()))
+		if err != nil {
+			return nil, fmt.Errorf("unable to read file %q: %w", entry.Name(), err)
 		}
+		var migration = Migration{
+			Filename:  entry.Name(),
+			Content:   string(content),
+			Direction: direction,
+		}
+		// fmt.Printf("File: %s\nContent:\n%s\n", entry.Name(), string(content))
+		migrations = append(migrations, migration)
 	}
-	if len(migrations) == 0 {
-		return nil, fmt.Errorf("no files found with ending '%s.sql'", direction)
-	}
-
 	return migrations, nil
 }
--- migration_test.go
+++ migration_test.go
@@ -1,19 +1,47 @@
 package postgres
 
 import (
+	"os"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 )
 
-func TestFetchFiles(t *testing.T) {
-	migrations, err := Migrations("example-migrations", "down")
-	require.NoError(t, err)
-	require.NotNil(t, migrations)
-	for _, migration := range migrations {
-		t.Logf("migration: %v (%v)\n%v\n",
-			migration.Filename,
-			migration.Direction,
-			migration.Content)
-	}
+func TestMigrations(t *testing.T) {
+	t.Run("embed up", func(t *testing.T) {
+		m, err := Migrations(migrationsDirEmbed, exampleMigrationsDir, "up")
+		require.NoError(t, err)
+		require.NotEmpty(t, m)
+		for _, migration := range m {
+			t.Logf("embed up: %s", migration.Filename)
+			t.Logf("content: %s", migration.Content)
+		}
+	})
+	t.Run("embed down", func(t *testing.T) {
+		m, err := Migrations(migrationsDirEmbed, exampleMigrationsDir, "down")
+		require.NoError(t, err)
+		require.NotEmpty(t, m)
+		for _, migration := range m {
+			t.Logf("embed down: %s", migration.Filename)
+			t.Logf("content: %s", migration.Content)
+		}
+	})
+	t.Run("local up", func(t *testing.T) {
+		m, err := Migrations(os.DirFS("."), exampleMigrationsDir, "up")
+		require.NoError(t, err)
+		require.NotEmpty(t, m)
+		for _, migration := range m {
+			t.Logf("local up: %s", migration.Filename)
+			t.Logf("content: %s", migration.Content)
+		}
+	})
+	t.Run("local down", func(t *testing.T) {
+		m, err := Migrations(os.DirFS("."), exampleMigrationsDir, "down")
+		require.NoError(t, err)
+		require.NotEmpty(t, m)
+		for _, migration := range m {
+			t.Logf("local down: %s", migration.Filename)
+			t.Logf("content: %s", migration.Content)
+		}
+	})
 }
